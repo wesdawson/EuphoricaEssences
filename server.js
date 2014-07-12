1b2fbb6083b7f8720890a67bd7dba2e60555928c
Wes <wesley.s.dawson@gmail.com>
2014-07-12 13:50:26 -0700

    index view added

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+    res.sendfile('./views/index.html');
+});
+ 
+app.get('/about', function(req, res) {
+    res.sendfile('./views/about.html');
+});
+ 
+app.get('/article', function(req, res) {
+    res.sendfile('./views/article.html');
+});
+
 app.get('/', function(req, res){
   res.send("Titty Winks");
 });
