03dafa3250e3ea2427f4dc327fff08626ea02f93
Wes <wesley.s.dawson@gmail.com>
2014-07-12 15:18:17 -0700

    bootstrap setup and public directory structure

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/public/js', express.static(path.join(__dirname, '/public/js')));
+app.use('/public/stylesheets', express.static(path.join(__dirname, '/public/stylesheets')));
+
 app.get('/', function(req, res) {
-    res.sendfile('./views/index.html');
+    res.sendfile('./public/views/index.html');
 });
  
-app.get('/about', function(req, res) {
-    res.sendfile('./views/about.html');
-});
- 
-app.get('/article', function(req, res) {
-    res.sendfile('./views/article.html');
-});
-
-
 app.listen(3000);
 console.log("You are listening on localhost:3000");
